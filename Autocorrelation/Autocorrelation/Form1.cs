de9aa18ed1844b233b05088cdad3d8861d8f4fc6
Belobrodsky <belobrodsky@yandex.ru>
2016-12-28 23:57:52 +0300

    Первый домашний комит для сохранения

---
+++ Autocorrelation/Autocorrelation/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autocorrelation
+{
+    public partial class Form1 : Form
+    {
+        List<double> inputVec;
+
+        //Ну ИМХО так удобнее сам на себе множить
+        List<double> inputVec2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            inputVec = new List<double>();
+
+            inputVec2 = new List<double>();
+        }
+
+        private void открытьИПосчитатьАКToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                StreamReader sr = new StreamReader(openFileDialog1.FileName);
+                string line = "123";
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+
+                    if (line != null)
+                    {
+                        //   MessageBox.Show(vecA.Count.ToString());
+                        inputVec.Add(Double.Parse(line));
+                        inputVec2.Add(Double.Parse(line));
+                    }
+                }
+                sr.Close();
+
+
+                StreamWriter sw = new StreamWriter(openFileDialog1.FileName + "_autocorr.csv");
+                while (inputVec.Count>0)
+                {
+                    sw.WriteLine(Correlation(inputVec, inputVec2));
+                    inputVec.RemoveAt(0);
+                    inputVec2.RemoveAt(inputVec2.Count-1);
+                }
+
+                sw.Close();
+
+
+
+            }
+        }
+
+
+        public double Correlation(List<double> a, List<double> b)
+        {
+
+            double sum = 0.0;
+            //пожразумевая что буду подсовывать сигнал один и тотже
+            for (int i = 0; i < a.Count; i++)
+            {
+                sum += a[i] * b[i];
+            }
+            return sum;
+        }
+
+
+
+    }
+}
---
+++ Autocorrelation/Autocorrelation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Autocorrelation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Autocorrelation")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3ecb7aa2-1c26-4548-ad66-9f93ef7013a5")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
