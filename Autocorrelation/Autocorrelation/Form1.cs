b23bf41db66f60fa26f087b71614b7837e19d34f
Vladimir Belobrodsky <belobrodsky@yandex.ru>
2016-12-29 09:21:18 +0300

    протестирована Автокорреляция с Пирсоном

--- Autocorrelation/Autocorrelation/Form1.cs
+++ Autocorrelation/Autocorrelation/Form1.cs
@@ -50,7 +50,7 @@ namespace Autocorrelation
                 StreamWriter sw = new StreamWriter(openFileDialog1.FileName + "_autocorr.csv");
                 while (inputVec.Count>0)
                 {
-                    sw.WriteLine(Correlation(inputVec, inputVec2));
+                    sw.WriteLine(PirsonCorrelation(inputVec, inputVec2));
                     inputVec.RemoveAt(0);
                     inputVec2.RemoveAt(inputVec2.Count-1);
                 }
@@ -62,6 +62,36 @@ namespace Autocorrelation
 
             }
         }
+
+
+        /// <summary>
+        /// Запрогаем формулу Пирсона от сюда https://ru.wikipedia.org/wiki/Корреляция
+        /// </summary>
+        /// <returns></returns>
+        public double PirsonCorrelation(List<double> X, List<double> Y)
+        {
+
+            double Xsr = X.Average();
+            double Ysr = Y.Average();
+
+            double sum_chislit = 0.0;
+
+            double kv_X_Xsr = 0.0;
+            double kv_Y_Ysr = 0.0;
+
+            for (int i = 0; i < X.Count; i++)
+            {
+
+                sum_chislit+=((X[i]-Xsr)*(Y[i]-Ysr));
+
+                kv_X_Xsr += (X[i] - Xsr) * (X[i] - Xsr);
+                kv_Y_Ysr += (Y[i] - Ysr) * (Y[i] - Ysr);
+
+            }
+
+            return sum_chislit/(Math.Sqrt(kv_X_Xsr)*Math.Sqrt(kv_Y_Ysr));
+        }
+
 
 
         public double Correlation(List<double> a, List<double> b)
